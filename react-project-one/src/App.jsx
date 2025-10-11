3061fb9b289eea96ba1fc3cfa6f55d7247ed7176
MOHD-SHAHVEZ <javedgaur57@gmail.com>
2025-10-11 16:23:28 +0530

    Day 1 Learning React Project One

---
+++ react-project-one/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(10)
+
+  return (
+    <>
+     <p>Hello Jii, My Name is Mohd Shahvez!</p>
+    </>
+  )
+}
+
+export default App
